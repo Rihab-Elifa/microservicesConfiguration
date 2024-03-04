c68d37e89d10c98b821fc90696ffa0bcdb93f58f
Rihab <elifarihab@gmail.com>
2024-03-04 13:42:49 +0100

    modify

--- profil/src/main/java/com/example/profil/ProfilApplication.java
+++ profil/src/main/java/com/example/profil/ProfilApplication.java
@@ -1,16 +1,15 @@
 package com.example.profil;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
-public class ProfilApplication {
+class ProfilApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProfilApplication.class, args);
 	}
 
